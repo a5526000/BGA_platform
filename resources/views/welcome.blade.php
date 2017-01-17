5d1ea2ec7d426c89399d62cb702bb47a0efb7b02
sunhh <sunhh@dnarails.com>
2017-01-17 15:54:57 +0800

    Hello laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Laravel
                 </div>
 
                 <div class="links">
